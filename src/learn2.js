c97a5123359932ae57357aee548809cf1ef59685
inoueyuji <sikogaku2593@gmail.com>
2024-12-09 17:08:04 +0900

    learn4

--- src/learn2.js
+++ src/learn2.js
@@ -86,5 +86,35 @@ let title = 'エンジニア';
 // name = '次郎';
 //→constで宣言した値に再宣言するとエラーが起きる Uncaught TypeError TypeError: Assignment to constant variable.
 
+//アロー関数
+const add = (a,b) => {
+    return a + b;
+}
+add(1,2);
 
-//
+const sin = (d,f) => d + f;
+//１つしか評価項目がない場合、returnの行を暗黙的にすることができる
+//あくまでも可読性を優先させて行の短縮を判断する
+sin(1,2);
+console.log(add(1,2));
+
+//引数が一つの時、(number)の（）も省略できる
+//下記が最も省略された書き方となる
+const dom = number => {
+    return 3 * number
+};
+console.log(dom(99999));
+
+const numbers = [ 1,2,3 ];
+
+//map関数アロー関数を使う場合
+let sun = numbers.map(number => 2 * number);
+console.log(sun);
+
+const profile = {
+    name: '太郎',
+    getName: function() {
+        return this.name;
+    }
+
+    };
